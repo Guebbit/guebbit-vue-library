b973b8e2be09ee6067e1a2be88140935e2f19716
guerzoni_andrea <guerzoni.andrea91@gmail.com>
2022-04-30 15:39:33 +0200

    init fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,11 @@
-export default {};
+// blocks
+import Panel from "@/components/blocks/Panel.vue";
+// cards
+import BookCard from "@/components/cards/BookCard.vue";
+import CreditCard from "@/components/cards/CreditCard.vue";
+
+export default {
+    Panel,
+    BookCard,
+    CreditCard
+};
